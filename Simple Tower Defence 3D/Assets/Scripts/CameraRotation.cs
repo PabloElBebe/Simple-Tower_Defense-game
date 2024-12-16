21e5fda838f00913c04a06c7a226e87a97742036
PabloElBebe <rinat_aminov_2006@mail.ru>
2024-12-16 18:00:08 +0300

    Update

---
+++ Simple Tower Defence 3D/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    private float _horizontalInput;
+    
+    private void Update()
+    {
+        Input();
+        Rotate();
+    }
+
+    private void Input()
+    {
+        _horizontalInput = UnityEngine.Input.GetAxis("Horizontal");
+    }
+    
+    private void Rotate()
+    {
+        float rotation = -_horizontalInput * Time.deltaTime * 150;
+        transform.Rotate(0, rotation, 0);
+    }
+}
