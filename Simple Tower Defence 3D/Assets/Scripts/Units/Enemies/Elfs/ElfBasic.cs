94006a7a4906e11be72d05d0c1e72d9e3b056024
PabloElBebe <rinat_aminov_2006@mail.ru>
2024-12-16 14:05:30 +0300

    Update

---
+++ Simple Tower Defence 3D/Assets/Scripts/Units/Enemies/Elfs/ElfBasic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ElfBasic : BasicEnemy, IFlammable
+{
+    public IEnumerator Flame()
+    {
+        float timer = 2;
+        
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            GetComponent<IDamagableWithEffects>().GetDamage(5);
+
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
---
+++ Simple Tower Defence 3D/Assets/Scripts/Units/Enemies/Elfs/ElfLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ElfLight : LightEnemy, IFlammable
+{
+    public IEnumerator Flame()
+    {
+        float timer = 2;
+        
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            GetComponent<IDamagableWithEffects>().GetDamage(5);
+
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
